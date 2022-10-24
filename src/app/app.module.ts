afbf16269c82149da1b30aabff8e910c45315b30
Dawudesign <contact@dawudesign.fr>
2022-10-24 13:31:59 +0200

    start step6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +23,11 @@ import { DashbordComponent } from './dashbord/dashbord.component'; // <-- NgMode
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
