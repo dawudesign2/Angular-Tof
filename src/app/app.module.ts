eb2cf1d9fa5c5234942c03e7fbbbbbd1875942e8
Dawudesign <contact@dawudesign.fr>
2022-10-24 13:47:17 +0200

    start step6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { DashbordComponent } from './dashbord/dashbord.component'; // <-- NgModel lives here
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { DashbordComponent } from './dashbord/dashbord.component'; // <-- NgMode
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashbordComponent
+    DashbordComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
